5860569524cb1cd451bec1a48caf3fdbffca4aff
Dirah255 <60676185+Dirah255@users.noreply.github.com>
2020-02-06 09:24:03 -0500

    Update Taverns.sql

--- Taverns.sql
+++ Taverns.sql
@@ -118,11 +118,11 @@ INSERT INTO Location
  St,
  Zip)
 VALUES
-    ('987 East Dr.','Ste G', 'NJ', 00254),
-    ('101 North St.', ,'PA', 19100),
-    ('123 W Well St.', , 'DE', 09050)
-    ('1 Imagination Ave.', ,'AK', 99998),
-    ('321 E LLEW St.', ,'DE', 05090);
+    ('987 East Dr.', 'Ste G', 'NJ', 00254),
+    ('101 North St.', '','PA', 19100),
+    ('123 W Well St.', '' , 'DE', 09050)
+    ('1 Imagination Ave.', '' ,'AK', 99998),
+    ('321 E LLEW St.', '' ,'DE', 05090);
 
 INSERT INTO Rat
 (Name)
