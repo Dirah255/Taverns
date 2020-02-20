bb257d346271aee37e719ee625d0faeab7db9607
Dirah255 <60676185+Dirah255@users.noreply.github.com>
2020-02-19 21:58:05 -0500

    Add files via upload

--- Guest_Class.sql
+++ Guest_Class.sql
@@ -4,6 +4,7 @@ DROP TABLE if exists Sale_Inventory_Update;
 DROP TABLE if exists Sale;
 DROP TABLE if exists Service;
 DROP TABLE if exists Inventory;
+DROP TABLE if exists Supply;
 DROP TABLE if exists Tavern;
 DROP TABLE if exists Owner;
 DROP TABLE if exists Role;
@@ -72,7 +73,12 @@ CREATE TABLE Tavern
 	LocationId int REFERENCES Location(ID),
 	OwnerId int NOT NULL REFERENCES Owner(ID),
 );
-
+CREATE TABLE Supply
+(	
+	ID INTEGER NOT NULL PRIMARY KEY IDENTITY,
+	Name varchar(40) NOT NULL,
+	UNIT varchar(40) NOT NULL,
+);
 
 CREATE TABLE Inventory
 (
@@ -82,7 +88,7 @@ CREATE TABLE Inventory
 	UpDated varchar(40) NOT NULL,
 	StatusID int NOT NULL REFERENCES Status(Id),
 	TavernID int REFERENCES Tavern(Id),
-	SupplyID int NOT NULL,
+	SupplyID int NOT NULL REFERENCES Supply(Id),
 );
 
 CREATE TABLE Service
@@ -116,7 +122,8 @@ CREATE TABLE Sale_Inventory_Update
 (
 	ID int NOT NULL PRIMARY KEY IDENTITY,
 	StatusID int REFERENCES Status(Id),
-	TavernID int REFERENCES Tavern(Id)
+	TavernID int REFERENCES Tavern(Id),
+	Price decimal (10,2) NOT NULL
 )
 
 CREATE TABLE Room_Stay
@@ -124,9 +131,9 @@ CREATE TABLE Room_Stay
 	SaleID int NOT NULL PRIMARY KEY IDENTITY,
 	GuestName varchar(40) NOT NULL,
 	RoomID int REFERENCES Rooms(Id),
-	DATEBOOKED varchar(40),
+	DATEBOOKED varchar(10),
 	NumNight int NOT NULL,
-	Rate int
+	Total int NOT NULL
 )
 
 INSERT INTO Status
@@ -209,6 +216,16 @@ VALUES
     ('We Side', 4, 4, 5),
     ('UpsideDown', 1, 5, 4);
 
+INSERT INTO Supply
+(Name, 
+ UNIT)
+VALUES
+	('Beer', 'barrel'),
+	('Bread', 'Loaf'),
+	('Wine', 'Cup'),
+	('Water', 'OZ'),
+	('potage', 'Bowl');
+
 INSERT INTO Inventory
 (Price,
  Qty,
@@ -243,7 +260,7 @@ Qty,
 Total,
 TavernID)
 VALUES
-	('Kitty', 2, 3, 1, 25, 5),
+	('Kitty', 2, 3, 10, 25, 5),
 	('Tiger', 3, 1, 1, 10, 5),
 	('Bubbles', 5, 4, 1, 100, 1),
 	('Prince', 1, 2, 1, 400, 2),
@@ -272,90 +289,148 @@ VALUES
 
 INSERT INTO Rooms
 (StatusID,
- TavernID)
+ TavernID,
+ Price)
  VALUES
-	(3,1),
-	(3,2),
-	(3,3),
-	(4,4),
-	(3,5),
-	(3,5),
-	(4,5),
-	(3,3),
-	(4,1),
-	(3,2),
-	(3,5),
-	(3,5);
+	(3,1,100),
+	(3,2,150),
+	(3,3,200),
+	(4,4,300),
+	(3,5,1500),
+	(3,5,1000),
+	(4,5,850),
+	(3,3,120.50),
+	(4,1,230),
+	(3,2,320),
+	(3,5,6300),
+	(3,5,80);
 
 INSERT INTO Room_Stay
 (GuestName, 
  RoomID,
  DATEBOOKED,
  NumNight,
- Rate)
+ Total)
 
 VALUES
 ('Joe Jack', 2, '2000-10-10', 1, 300),
-('Billy Bob', 3, '2019-03-07-8975', 5, 500),
+('Billy Bob', 3, '2019-07-05', 5, 500),
 ('Jesse Jones', 6, '1945-09-07', 54, 800),
 ('Al Alferd',4, '2003-12-30', 6, 50),
 ('Zac Zoey', 9, '2010-06-09', 7, 1500),
 ('Joe Jack', 7, '2019-08-12', 6, 5600),
 ('Abcd Wyz', 11, '2017-10-25', 1, 5600),
-('Henry Hetz', 1, '2018-100-02', 3, 30),
+('Henry Hetz', 1, '2018-10-02', 3, 30),
 ('Trevor Thompson', 5, '2018-09-03', 9, 450),
 ('Henry Hetz ll', 10, '2019-15-19', 10, 400),
-('Vern Vattle', 8, '2019-12-303', 14, 890),
+('Vern Vattle', 8, '2019-12-30', 14, 890),
 ('Henry He', 1, '2020-01-20', 3, 30);
 
---#1 HW (I took this as anyone who had a roleid)
-SELECT * FROM Owner WHERE RoleId is NOT NULL
+--#1
+SELECT * FROM Owner
+LEFT JOIN Role ON (Owner.RoleId = Role.Id)
 
 --#2
-SELECT * FROM Owner 
-LEFT JOIN Tavern ON (Owner.RoleId = Tavern.OwnerId) WHERE RoleId is NOT NULL
-
+SELECT COUNT(GUEST_CLASS.ClassID) as StudentCount FROM GUEST_CLASS
+LEFT JOIN CLASS ON(CLASS.ID = GUEST_CLASS.ClassID) 
+GROUP BY GUEST_CLASS.ClassID
+ 
 --#3
-SELECT * FROM GUEST ORDER BY Name ASC
+SELECT Guest.Name, Class.Name, (CASE WHEN ClassLvls < 6 THEN 'Beginner'
+			WHEN ClassLvls BETWEEN 5 AND 10 THEN 'Intermediate' 
+			WHEN ClassLvls> 10 THEN 'Expert' END) as ClassLvl  FROM GUEST_CLASS
+LEFT JOIN GUEST ON (GUEST_CLASS.GuestID = GUEST.ID)
+LEFT JOIN CLASS ON (GUEST_CLASS.ClassID = CLASS.ID)
 
 --#4
-SELECT TOP 10 Sale.GuestName, Service.Price, Service.Name as Service_Name FROM Sale 
-LEFT JOIN Service ON (Sale.ServiceId = Service.Id)
-ORDER BY Price DESC
+GO
+IF OBJECT_ID (N'LvlGroup') IS NOT NULL
+DROP FUNCTION LvlGroup;
+GO
+CREATE FUNCTION dbo.LvlGroup (@lvl int)
+Returns varchar (30)
+AS
+BEGIN
+DECLARE @LvlString varchar(30)
+SELECT 
+	@LvlString = CASE WHEN @lvl < 6 THEN 'Beginner'
+			WHEN @lvl BETWEEN 5 AND 10 THEN 'Intermediate' 
+			WHEN @lvl> 10 THEN 'Expert' END
+Return @LvlString 
+
+END;
+
+GO
+
+SELECT dbo.LvlGroup(11) 
 
 --#5
-SELECT GUEST.Name, COUNT(GuestID) as Class_Count FROM GUEST_CLASS
-LEFT JOIN GUEST ON (GUEST.ID = GuestID) 
-GROUP BY Guest.Name
-HAVING COUNT(GuestID) > 1
+GO
+IF OBJECT_ID (N'IFBooked') IS NOT NULL
+DROP FUNCTION IfBooked;
+GO
+CREATE FUNCTION IfBooked (@date varchar(20))
+Returns Table
+AS
+Return
+(
+SELECT SaleID, Room_Stay.GuestName, RoomID, DATEBOOKED,Tavern.Name as TavernName From Room_Stay 
+LEFT JOIN Rooms ON (Room_Stay.RoomID = Rooms.ID)
+LEFT JOIN Tavern ON (Rooms.TavernID = Tavern.ID)
+where @date != Room_Stay.DATEBOOKED
+);
+GO
+SELECT * FROM IfBooked ('2000-10-10')
 
 --#6
-SELECT GUEST.Name, Max(GUEST_CLASS.ClassLvls) as Max_Class_Lvl FROM GUEST_CLASS 
-LEFT JOIN GUEST ON (GUEST.ID = GUEST_CLASS.GuestID)
-GROUP BY Guest.Name
-HAVING COUNT(GuestID) > 1 
+GO
+IF OBJECT_ID (N'Range') IS NOT NULL
+DROP FUNCTION Range;
+GO
+CREATE FUNCTION Range (@min int,@max int)
+Returns Table
+AS
+Return
+(
+SELECT RoomID, Room_Stay.Total, Tavern.Name as TavernName From Room_Stay 
+LEFT JOIN Rooms ON (Room_Stay.RoomID = Rooms.ID)
+LEFT JOIN Tavern ON (Rooms.TavernID = Tavern.ID)
+where Room_Stay.Total BETWEEN @min AND @max
+);
+GO
+SELECT * FROM Range(100,1500)
 
 --#7
-SELECT GUEST.Name, MAX(GUEST_CLASS.ClassLvls) AS Max_Class_Lvl FROM GUEST_CLASS 
-LEFT JOIN GUEST ON (GUEST.ID = GUEST_CLASS.GuestID) 
-GROUP BY GUEST.Name
+GO 
+DROP PROCEDURE IF EXISTS InsertIntoRoomsMin
 
---#8
-SELECT * FROM Room_Stay 
-WHERE DATEBOOKED BETWEEN '2000-05-05' AND '2015-05-05'
+GO
+CREATE PROCEDURE InsertIntoRoomsMin
+@max decimal (10,2),
+@min decimal (10,2)
+AS
+BEGIN 
+DECLARE @tavid int
+DECLARE @newprice decimal (10,2)
+DECLARE @roommin decimal (10,2)
+SET NOCOUNT ON;
+SELECT 
+@roommin = MIN (Rooms.Price)From Rooms
+LEFT JOIN Tavern ON (Rooms.TavernID = Tavern.ID)
+where Rooms.Price BETWEEN @min AND @max
+SELECT @tavid = FLOOR(RAND()*(5-1+1))+1
+SELECT @newprice = (@roommin - 0.01)
+INSERT INTO Rooms (StatusID, TavernID, Price)
+VALUES (3, @tavid, @newprice) 
+END
 
---#9
-SELECT concat ('Create Table ', Table_name, '(') as queryPiecee from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Tavern' 
-UNion All
-SELECT CONCAT (cols.COLUMN_NAME,' ', DATA_TYPE,  (
-	CASE WHEN DATA_TYPE = 'varchar' then concat(  '(', CHARACTER_MAXIMUM_LENGTH, ')', ',' )end),
-   (CASE WHEN refCon.CONSTRAINT_NAME IS NOT NULL then concat( ' ','FOREIGN KEY REFERENCES', ' ', conKey.TABLE_NAME, ' ', '(', keys.COLUMN_NAME, ')') end),
-   (CASE WHEN keys.CONSTRAINT_NAME LIKE'PK%'then concat( ' ','PRIMARY KEY IDENTITY') end))
-	FROM INFORMATION_SCHEMA.COLUMNS as cols 
-LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE as keys ON (keys.TABLE_NAME = cols.TABLE_NAME and keys.COLUMN_NAME = cols.COLUMN_NAME)
-LEFT JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS as refCon ON (refCon.CONSTRAINT_NAME = keys.CONSTRAINT_NAME)
-LEFT JOIN (SELECT DISTINCT CONSTRAINT_NAME, TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE) as conKey ON (conKey.CONSTRAINT_NAME = refCon.UNIQUE_CONSTRAINT_NAME)
-WHERE cols.TABLE_NAME = 'Tavern'
-UNION ALL
-SELECT ')'; 
+GO
+EXEC dbo.InsertIntoRoomsMin @max=1500, @min=50;
+SELECT * FROM Rooms 
+
+
+
+
+
+
 
